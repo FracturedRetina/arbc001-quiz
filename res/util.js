0042821831d15bdf52e751e3adc7d18973c5915f
Evan Shimoniak <eshimoniak@gmail.com>
2019-03-15 21:13:58 -0700

    Added pronoun loader

--- res/util.js
+++ res/util.js
@@ -2,6 +2,18 @@ const LONG = "ايو";
 const SHORT = "َُِ";
 const OTHER = "ّْ";
 
+const PERSON_FIRST = 1;
+const PERSON_SECOND = 2;
+const PERSON_THIRD = 3;
+
+const GENDER_NONE = 'n';
+const GENDER_MASCULINE = 'm';
+const GENDER_FEMININE = 'f';
+
+const SINGULAR = 1;
+const DUAL = 2;
+const PLURAL = 3;
+
 function loadWords(data) {
 	var loaded = [];
 	var lines = data.split("\n");
@@ -12,6 +24,34 @@ function loadWords(data) {
 		loaded.push(entry);
 	}
 	return loaded;
+}
+
+function loadPronouns(data) {
+	var ret = {};
+	var lines = data.split("\n");
+	for (var i = 0; i < lines.length; i++) {
+		var splitLn = lines[i].split(',');
+		for (var j = 0; j < splitLn.length; j++) {
+			if (j == 0) {
+				if (ret[PERSON_FIRST] == undefined) {
+					ret[PERSON_FIRST] = {};
+				}
+				if (ret[PERSON_FIRST][GENDER_NONE] == undefined) {
+					ret[PERSON_FIRST][GENDER_NONE] = {};
+				}
+				ret[PERSON_FIRST][GENDER_NONE][i + 1] = splitLn[j];
+			} else {
+				if (ret[Math.floor((j - 1) / 2) + 2] == undefined) {
+					ret[Math.floor((j - 1) / 2) + 2] = {};
+				}
+				if (ret[Math.floor((j - 1) / 2) + 2][(j % 2 == 1) ? GENDER_MASCULINE : GENDER_FEMININE] == undefined) {
+					ret[Math.floor((j - 1) / 2) + 2][(j % 2 == 1) ? GENDER_MASCULINE : GENDER_FEMININE] = {};
+				}
+				ret[Math.floor((j - 1) / 2) + 2][(j % 2 == 1) ? GENDER_MASCULINE : GENDER_FEMININE][i + 1] = splitLn[j];
+			}
+		}
+	}
+	return ret;
 }
 
 function countVowels(word) {
